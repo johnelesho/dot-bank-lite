c6e80ea43fd179d20beff6b1eee3a818d3154ac5
crudeboy <jahswill.apata@gmail.com>
2024-11-11 18:39:59 +0100

    first commit plane spring project

---
+++ src/main/java/Jazz/dot_lite/DotLiteApplication.java
@@ -0,0 +1,13 @@
+package Jazz.dot_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Jazz/dot_lite/DotLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package Jazz.dot_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
